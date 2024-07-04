68e6a9d76f1f6eebe754aeec8217c3aa3b65f9cc
Sabat Agung Sirait <73856124+sabatagung@users.noreply.github.com>
2024-07-04 17:34:39 +0700

    Add files via upload

---
+++ spring-mq-apache/src/main/java/com/example/spring_mq_apache/SpringMqApacheApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_mq_apache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMqApacheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMqApacheApplication.class, args);
+	}
+
+}
---
+++ spring-mq-apache/src/test/java/com/example/spring_mq_apache/SpringMqApacheApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_mq_apache;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMqApacheApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
